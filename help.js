465fc3e612a5dca9d36990b4987097af2b0bff20
dstapert <dstapert1@gmail.com>
2025-05-03 00:51:59 -0500

    Version 1.4.6

--- help.js
+++ help.js
@@ -1,20 +1,9 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    console.log(this);
-    var content = this.nextElementSibling;
-    console.log(content);
-    if (content){
-      this.classList.toggle("active");
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    }else{
+document.querySelectorAll(".collapsible").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const content = btn.nextElementSibling;
+    if (content) {
+      btn.classList.toggle("active");
+      content.style.display = content.style.display === "block" ? "none" : "block";
     }
-    
   });
-}
+});
